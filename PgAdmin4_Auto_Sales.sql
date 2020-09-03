afa5c0e7e40aa3e70570690b3307558bad0cb26a
Kelly Curtiss <klcurtiss@pthsd.net>
2020-09-02 22:53:01 -0400

    submit

---
+++ PgAdmin4_Auto_Sales.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Auto_Sales (
+index INT PRIMARY KEY,
+year varchar,
+make varchar,
+model varchar,
+category_list varchar,
+Q2_2020	varchar,
+Q2_2019 varchar,
+Year_to_Date varchar,
+Year_to_Date_Previous varchar,
+Change float
+);
+
+select * from Auto_Sales
